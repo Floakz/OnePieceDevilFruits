13c858dae17e92caabdbfb4875ea825ff6dcc93e
Floakz <sergioamoreira@sapo.pt>
2025-09-02 16:42:12 +0100

    add email
    -Add email to footer to get feedback

--- src/App.jsx
+++ src/App.jsx
@@ -210,6 +210,7 @@ function App() {
 
             <footer>
                 <h3>ONE PIECE DEVIL FRUITS . com</h3>
+                <span className='emailFooter'>Anything missing or wrong?<br /><span> info@onepiecedevilfruits.com</span></span>
                 <a href="#hero-title">back to the top</a>
             </footer>
         </>
